8ac2ca2a44921fe026ec03877b973417edfcbbff
cmendoza-dev <carlos.mendoza.s@tecsup.edu.pe>
2024-06-18 17:50:08 -0500

    Subir cambios

--- Componentes/especialidades.php
+++ Componentes/especialidades.php
@@ -1,127 +1,126 @@
 <div class="icon-grid-container">
-        <div class="icon-grid-title">Especialidades</div>
-        <div class="icon-grid">
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-home"></i>
-                    <div class="icon-caption">Home</div>
-                </a>
-            </div>
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-user"></i>
-                    <div class="icon-caption">User</div>
-                </a>
-            </div>
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-envelope"></i>
-                    <div class="icon-caption">Envelope</div>
-                </a>
-            </div>
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-phone"></i>
-                    <div class="icon-caption">Phone</div>
-                </a>
-            </div>
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-cog"></i>
-                    <div class="icon-caption">Settings</div>
-                </a>
-            </div>
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-heart"></i>
-                    <div class="icon-caption">Heart</div>
-                </a>
-            </div>
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-star"></i>
-                    <div class="icon-caption">Star</div>
-                </a>
-            </div>
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-comments"></i>
-                    <div class="icon-caption">Comments</div>
-                </a>
-            </div>
+    <div class="icon-grid-title">Especialidades</div>
+    <div class="icon-grid">
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fa-solid fa-smoking"></i>
+                <div class="icon-caption">Adicciones</div>
+            </a>
+        </div>
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fas fa-user"></i>
+                <div class="icon-caption">Ansiedad</div>
+            </a>
+        </div>
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fa-solid fa-triangle-exclamation"></i>
+                <div class="icon-caption">Atención</div>
+            </a>
+        </div>
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fas fa-heart"></i>
+                <div class="icon-caption">Autoestima</div>
+            </a>
+        </div>
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fa-solid fa-person-breastfeeding"></i>
+                <div class="icon-caption">Crianza</div>
+            </a>
+        </div>
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fa-solid fa-battery-half"></i>
+                <div class="icon-caption">Depresión</div>
+            </a>
+        </div>
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fas fa-star"></i>
+                <div class="icon-caption">Enfermedades
+                    crónicas</div>
+            </a>
+        </div>
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fas fa-comments"></i>
+                <div class="icon-caption">Estrés</div>
+            </a>
+        </div>
 
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-home"></i>
-                    <div class="icon-caption">Home</div>
-                </a>
-            </div>
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-user"></i>
-                    <div class="icon-caption">User</div>
-                </a>
-            </div>
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-envelope"></i>
-                    <div class="icon-caption">Envelope</div>
-                </a>
-            </div>
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-phone"></i>
-                    <div class="icon-caption">Phone</div>
-                </a>
-            </div>
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-cog"></i>
-                    <div class="icon-caption">Settings</div>
-                </a>
-            </div>
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-heart"></i>
-                    <div class="icon-caption">Heart</div>
-                </a>
-            </div>
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-star"></i>
-                    <div class="icon-caption">Star</div>
-                </a>
-            </div>
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-comments"></i>
-                    <div class="icon-caption">Comments</div>
-                </a>
-            </div>
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fas fa-home"></i>
+                <div class="icon-caption">Impulsividad</div>
+            </a>
+        </div>
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fas fa-user"></i>
+                <div class="icon-caption">Top</div>
+            </a>
+        </div>
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fas fa-envelope"></i>
+                <div class="icon-caption">Ira</div>
+            </a>
+        </div>
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fa-solid fa-heart-crack"></i>
+                <div class="icon-caption">Terapia de Pareja</div>
+            </a>
+        </div>
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fa-solid fa-fire-flame-curved"></i>
+                <div class="icon-caption">Sexualidad</div>
+            </a>
+        </div>
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fa-light fa-star-half"></i>
+                <div class="icon-caption">Riesgo suicida</div>
+            </a>
+        </div>
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fas fa-star"></i>
+                <div class="icon-caption">Sentido de vida</div>
+            </a>
+        </div>
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fas fa-comments"></i>
+                <div class="icon-caption">Orientación
+                    Vocacional</div>
+            </a>
+        </div>
 
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-home"></i>
-                    <div class="icon-caption">Home</div>
-                </a>
-            </div>
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-user"></i>
-                    <div class="icon-caption">User</div>
-                </a>
-            </div>
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-envelope"></i>
-                    <div class="icon-caption">Envelope</div>
-                </a>
-            </div>
-            <div class="icon-item">
-                <a href="blog.php">
-                    <i class="fas fa-phone"></i>
-                    <div class="icon-caption">Phone</div>
-                </a>
-            </div>
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fa-solid fa-bed"></i>
+                <div class="icon-caption">Problemas
+                    de sueño</div>
+            </a>
+        </div>
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fa-solid fa-hand-holding-heart"></i>
+                <div class="icon-caption">Problemas
+                    alimenticios</div>
+            </a>
+        </div>
+        <div class="icon-item">
+            <a href="blog.php">
+                <i class="fa-solid fa-people-arrows"></i>
+                <div class="icon-caption">Relaciones
+                    Interpersonales</div>
+            </a>
         </div>
     </div>
+</div>
--- Componentes/header.php
+++ Componentes/header.php
@@ -1,7 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+    <!-- Importación de Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <!-- Importación de Normalize.css -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" rel="stylesheet">
+</head>
 <header>
-    <nav>
+    <nav class="container">
         <div class="logo">
-            <img src="img/logo-actual.png" alt="Logo de la Empresa" class="logo-img">
+            <img src="img/logo-actual.png" alt="Contigo Voy" class="logo-img">
         </div>
         <div class="menu-icon" id="menu-icon">
             <span></span>
@@ -11,9 +23,9 @@
         <ul class="nav-links" id="nav-links">
             <li><a href="index.php">Inicio</a></li>
             <li><a href="blog.php">Blog</a></li>
-            <li><a href="psicologos.php">Psicologos</a></li>
+            <li><a href="psicologos.php">Psicólogos</a></li>
             <li class="dropdown">
-                <a href="../ContigoVoy/login.php">Iniciar Sesion</a>
+                <a href="../ContigoVoy/login.php">Iniciar Sesión</a>
             </li>
             <li><a href="#">Reservar Cita</a></li>
         </ul>
