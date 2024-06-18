d4fd29ce7aa1632aaa42b85d4a6e111a181d5181
cmendoza-dev <carlos.mendoza.s@tecsup.edu.pe>
2024-06-18 14:48:53 -0500

    rutasgenerales

--- Crud/Paciente/guardarAreaFamiliar.php
+++ Crud/Paciente/guardarAreaFamiliar.php
@@ -1,5 +1,8 @@
 <?php
-require_once("/home3/ghxumdmy/public_html/gestion-contigo-voy-com/Controlador/Paciente/ControllerPaciente.php");
+include 'config/config.php';
+require_once BASE_PATH . 'Controlador/Paciente/ControllerPaciente.php';
+// require_once BASE_PATH_WEB . 'Controlador/Paciente/ControllerPaciente.php';
+// require_once("/home3/ghxumdmy/public_html/gestion-contigo-voy-com/Controlador/Paciente/ControllerPaciente.php");
 $obj = new usernameControlerPaciente();
 $obj->guardarAreaFamiliar($_POST['IdPaciente'], $_POST['NomPadre'],$_POST['EstadoPadre'], $_POST['NomMadre'],$_POST['EstadoMadre'],$_POST['NomApoderado'],$_POST['EstadoApoderado'], $_POST['CantHermanos'], $_POST['CantHijos'], $_POST['IntegracionFamiliar'], $_POST['HistorialFamiliar']);
 ?>
--- Crud/Paciente/guardarAtencPaciente.php
+++ Crud/Paciente/guardarAtencPaciente.php
@@ -1,4 +1,7 @@
 <?php
-require_once("/home3/ghxumdmy/public_html/gestion-contigo-voy-com/Controlador/Paciente/ControllerPaciente.php");
+include 'config/config.php';
+require_once BASE_PATH . 'Controlador/Paciente/ControllerPaciente.php';
+// require_once BASE_PATH_WEB . 'Controlador/Paciente/ControllerPaciente.php';
+// require_once("/home3/ghxumdmy/public_html/gestion-contigo-voy-com/Controlador/Paciente/ControllerPaciente.php");
 $obj = new usernameControlerPaciente();
 $obj->guardarAtencPac($_POST['IdPaciente'], $_POST['IdEnfermedad'], $_POST['MotivoConsulta'], $_POST['FormaContacto'], $_POST['Diagnostico'], $_POST['Tratamiento'],$_POST['Observacion'],$_POST['UltimosObjetivos']);?>
