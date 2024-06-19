f018115451f98238a73ce3823b9b6519e674d777
Jorge-12319 <luisjorgejorge191919@hotmail.com>
2024-06-19 11:05:30 -0500

    Integrando Usuarios modelo, controlador, vista

---
+++ Controlador/UsuariosController.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'Modelo/UsuarioModel.php';
+
+class UsuariosController {
+    private $model;
+
+    public function __construct() {
+        $this->model = new UsuarioModel();
+    }
+
+    public function mostrarUsuarios() {
+        return $this->model->getUsuarios();
+    }
+
+    public function buscarPorEmail($email) {
+        return $this->model->buscarPorEmail($email);
+    }
+
+    public function agregarUsuario($email, $password, $fotoPerfil, $rol) {
+        return $this->model->agregarUsuario($email, $password, $fotoPerfil, $rol);
+    }
+
+    public function actualizarUsuario($id, $email, $password, $fotoPerfil, $rol) {
+        return $this->model->actualizarUsuario($id, $email, $password, $fotoPerfil, $rol);
+    }
+
+    public function eliminarUsuario($id) {
+        return $this->model->eliminarUsuario($id);
+    }
+}
+?>
