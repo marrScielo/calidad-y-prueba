ddee8cb5dab3e636e2ded929b9026e97937295ab
calvarado0210 <christian.amazing.it@gmail.com>
2024-06-19 11:13:11 -0500

    cambios

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,7 +1,7 @@
 <?php
 class conexion{
     private $host="localhost";
-    private $dbname="psicologiamaster";
+    private $dbname="contigovoy2";
     private $user="root";
     private $password="";
 
