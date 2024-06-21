f3ab6da6d0df7c25b4c282d01047de841fcca1b7
cmendoza-dev <carlos.mendoza.s@tecsup.edu.pe>
2024-06-21 15:03:16 -0500

    crear psicologos

---
+++ usuarios.sql
@@ -0,0 +1,41 @@
+-- Alexandra Sharmely Chalco Estrella
+INSERT INTO `usuarios` (`email`, `email_verified_at`, `password`, `rol`, `fotoPerfil`, `remember_token`, `created_at`, `updated_at`)
+VALUES ('alexandrasharmelychalcoestrell@gmail.com', '', '&R5H61+r', 'psicologo', 'https://acortar.link/8SUru0', '', '', '');
+
+INSERT INTO `psicologo` (`NombrePsicologo`, `Passwords`, `FechaRegistro`, `Usuario`, `celular`, `video`, `email`, `usuario_id`)
+VALUES ('Alexandra Sharmely Chalco Estrella', '&R5H61+r', '', 'alexandra.chalco2024', '931886519', 'https://www.youtube.com/watch?v=KVakLLMBQxg', 'alexandrasharmelychalcoestrell@gmail.com', LAST_INSERT_ID());
+
+-- Camila Dariana Pecho Espinoza
+INSERT INTO `usuarios` (`email`, `email_verified_at`, `password`, `rol`, `fotoPerfil`, `remember_token`, `created_at`, `updated_at`)
+VALUES ('cami2000pecho@gmail.com', '', 'd90v[YU3', 'psicologo', 'https://acortar.link/aIza9l', '', '', '');
+
+INSERT INTO `psicologo` (`NombrePsicologo`, `Passwords`, `FechaRegistro`, `Usuario`, `celular`, `video`, `email`, `usuario_id`)
+VALUES ('Camila Dariana Pecho Espinoza', 'd90v[YU3', '', 'camila.pecho2024', '925663986', 'https://www.youtube.com/watch?v=KVakLLMBQxg', 'cami2000pecho@gmail.com', LAST_INSERT_ID());
+
+-- Eunice Andrea Landa Panez
+INSERT INTO `usuarios` (`email`, `email_verified_at`, `password`, `rol`, `fotoPerfil`, `remember_token`, `created_at`, `updated_at`)
+VALUES ('andrealand1878@gmail.com', '', 'Dw"92@54', 'psicologo', 'https://acortar.link/aIza9l', '', '', '');
+
+INSERT INTO `psicologo` (`NombrePsicologo`, `Passwords`, `FechaRegistro`, `Usuario`, `celular`, `video`, `email`, `usuario_id`)
+VALUES ('Eunice Andrea Landa Panez', 'Dw"92@54', '', 'eunice.landa2024', '923610434', 'https://www.youtube.com/watch?v=KVakLLMBQxg', 'andrealand1878@gmail.com', LAST_INSERT_ID());
+
+-- Gerardine Estefany Cipriano Ollero
+INSERT INTO `usuarios` (`email`, `email_verified_at`, `password`, `rol`, `fotoPerfil`, `remember_token`, `created_at`, `updated_at`)
+VALUES ('gerardine.cip.ok@gmail.com', '', 'T88R6+2h', 'psicologo', 'https://acortar.link/aIza9l', '', '', '');
+
+INSERT INTO `psicologo` (`NombrePsicologo`, `Passwords`, `FechaRegistro`, `Usuario`, `celular`, `video`, `email`, `usuario_id`)
+VALUES ('Gerardine Estefany Cipriano Ollero', 'T88R6+2h', '', 'gerardine.cipriano2024', '928964665', 'https://www.youtube.com/watch?v=KVakLLMBQxg', 'gerardine.cip.ok@gmail.com', LAST_INSERT_ID());
+
+-- Liz Rosa Vicuña Onofre
+INSERT INTO `usuarios` (`email`, `email_verified_at`, `password`, `rol`, `fotoPerfil`, `remember_token`, `created_at`, `updated_at`)
+VALUES ('lizrosavicunaonofre@gmail.com', '', 'J3n01£2K', 'psicologo', 'https://acortar.link/aIza9l', '', '', '');
+
+INSERT INTO `psicologo` (`NombrePsicologo`, `Passwords`, `FechaRegistro`, `Usuario`, `celular`, `video`, `email`, `usuario_id`)
+VALUES ('Liz Rosa Vicuña Onofre', 'J3n01£2K', '', 'liz.vicuña2024', '916994281', 'https://www.youtube.com/watch?v=KVakLLMBQxg', 'lizrosavicunaonofre@gmail.com', LAST_INSERT_ID());
+
+-- Alexandra Senka Castro Gutiérrez
+-- INSERT INTO `usuarios` (`email`, `email_verified_at`, `password`, `rol`, `fotoPerfil`, `remember_token`, `created_at`, `updated_at`)
+-- VALUES ('', '', '81/nO5i77888', 'psicologo', 'https://acortar.link/aIza9l', '', '', '');
+
+-- INSERT INTO `psicologo` (`NombrePsicologo`, `Passwords`, `FechaRegistro`, `Usuario`, `celular`, `video`, `email`, `usuario_id`)
+-- VALUES ('Alexandra Senka Castro Gutiérrez', '81/nO5i77888', '', 'alexandra.castro2024', '946946957', 'https://www.youtube.com/watch?v=KVakLLMBQxg', '', LAST_INSERT_ID());
