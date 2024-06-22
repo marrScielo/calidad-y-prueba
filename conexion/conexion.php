7da84219892d2c8af8e2dc8e48f825c142aad4df
cmendoza-dev <carlos.mendoza.s@tecsup.edu.pe>
2024-06-22 11:45:56 -0500

    algunos cmabios

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -4,19 +4,18 @@ class conexion{
     // private $username = "root";
     // private $password = "";
     // private $database = "contigovoy3";
-    // private $conn;
 
     private $servername = "localhost";
     private $username = "ghxumdmy_psicologoapk";
     private $password = "Psicologo123";
     private $database = "ghxumdmy_psicologia";
-    private $conn;
+
 
     public function conexion(){
         try{
-            $this->conn = new PDO("mysql:host={$this->servername};dbname={$this->database}", $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-           
+            $PDO=new PDO("mysql:host=".$this->servername.";dbname=".$this->database,$this->username,$this->password);
+            $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $PDO;
 
         }catch(PDOException $e){
             return $e->getMessage();
