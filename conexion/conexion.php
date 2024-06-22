993f343ad7afdd51599dad144f151b697e1cb176
cmendoza-dev <carlos.mendoza.s@tecsup.edu.pe>
2024-06-22 11:38:55 -0500

    algunos cmabios

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,22 +1,22 @@
 <?php
 class conexion{
-    private $servername = "localhost";
-    private $username = "root";
-    private $password = "";
-    private $database = "contigovoy3";
-    private $conn;
+    // private $servername = "localhost";
+    // private $username = "root";
+    // private $password = "";
+    // private $database = "contigovoy3";
+    // private $conn;
 
-    //private $servername = "localhost";
-    //private $username = "ghxumdmy_psicologoapk";
-    //private $password = "Psicologo123";
-    //private $database = "ghxumdmy_psicologia";
-    //private $conn;
+    private $servername = "localhost";
+    private $username = "ghxumdmy_psicologoapk";
+    private $password = "Psicologo123";
+    private $database = "ghxumdmy_psicologia";
+    private $conn;
 
     public function conexion(){
         try{
-            $PDO=new PDO("mysql:host=".$this->host.";dbname=".$this->dbname,$this->user,$this->password);
-            $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $PDO;
+            $this->conn = new PDO("mysql:host={$this->servername};dbname={$this->database}", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+           
 
         }catch(PDOException $e){
             return $e->getMessage();
