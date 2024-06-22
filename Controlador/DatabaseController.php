fbbee6e64cd943a2ce125c94a0dda9e4a65e629b
cmendoza-dev <carlos.mendoza.s@tecsup.edu.pe>
2024-06-22 11:47:57 -0500

    algunos cmabios

--- Controlador/DatabaseController.php
+++ Controlador/DatabaseController.php
@@ -7,12 +7,6 @@ class DatabaseController {
     // private $database = "contigovoy3";
     // private $conn;
 
-    // private $servername = "localhost";
-    // private $username = "ghxumdmy_psicologoapk";
-    // private $password = "Psicologo123";
-    // private $database = "ghxumdmy_psicologiaMaster";
-    // private $conn;
-
     private $servername = "localhost";
     private $username = "ghxumdmy_psicologoapk";
     private $password = "Psicologo123";
