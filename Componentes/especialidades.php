415413381267c73a1b37e40ab2b39f98397cbb02
juansf <116114043+Indezxz@users.noreply.github.com>
2024-06-24 12:10:49 -0500

    Corregir punto 3 y 4.

--- Componentes/especialidades.php
+++ Componentes/especialidades.php
@@ -8,7 +8,7 @@
             </a>
         </div>
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-emoji-astonished" viewBox="0 0 16 16">
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
                     <path d="M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5M4.884 4.022a2 2 0 0 1 1.458-.048.5.5 0 0 0 .316-.948 3 3 0 0 0-2.167.077 3.1 3.1 0 0 0-.773.478q-.036.03-.07.064l-.002.001a.5.5 0 0 0 .707.708l-.001.002.001-.002a2 2 0 0 1 .122-.1 2 2 0 0 1 .41-.232Zm6.232 0a2 2 0 0 0-1.458-.048.5.5 0 1 1-.316-.948 3 3 0 0 1 2.168.077 3 3 0 0 1 .773.478l.07.064v.001a.5.5 0 0 1-.706.708l.002.002-.002-.002a2 2 0 0 0-.122-.1 2 2 0 0 0-.41-.232ZM8 10c-.998 0-1.747.623-2.247 1.246-.383.478.08 1.06.687.98q1.56-.202 3.12 0c.606.08 1.07-.502.687-.98C9.747 10.623 8.998 10 8 10" />
@@ -17,19 +17,19 @@
             </a>
         </div>
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
                 <i class="fa-solid fa-triangle-exclamation"></i>
                 <div class="icon-caption">Atención</div>
             </a>
         </div>
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
                 <i class="fas fa-heart"></i>
                 <div class="icon-caption">Autoestima</div>
             </a>
         </div>
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
                     <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
                 </svg>
@@ -37,20 +37,20 @@
             </a>
         </div>
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
                 <i class="fa-solid fa-battery-half"></i>
                 <div class="icon-caption">Depresión</div>
             </a>
         </div>
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
                 <i class="fas fa-star"></i>
                 <div class="icon-caption">Enfermedades
                     crónicas</div>
             </a>
         </div>
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
             <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-emoji-frown" viewBox="0 0 16 16">
   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
   <path d="M4.285 12.433a.5.5 0 0 0 .683-.183A3.5 3.5 0 0 1 8 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.5 4.5 0 0 0 8 9.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5"/>
@@ -60,13 +60,13 @@
         </div>
 
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
                 <i class="fas fa-home"></i>
                 <div class="icon-caption">Impulsividad</div>
             </a>
         </div>
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-square-fill" viewBox="0 0 16 16">
                     <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2z" />
                 </svg>
@@ -74,7 +74,7 @@
             </a>
         </div>
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-emoji-angry" viewBox="0 0 16 16">
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
                     <path d="M4.285 12.433a.5.5 0 0 0 .683-.183A3.5 3.5 0 0 1 8 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.5 4.5 0 0 0 8 9.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683m6.991-8.38a.5.5 0 1 1 .448.894l-1.009.504c.176.27.285.64.285 1.049 0 .828-.448 1.5-1 1.5s-1-.672-1-1.5c0-.247.04-.48.11-.686a.502.502 0 0 1 .166-.761zm-6.552 0a.5.5 0 0 0-.448.894l1.009.504A1.94 1.94 0 0 0 5 6.5C5 7.328 5.448 8 6 8s1-.672 1-1.5c0-.247-.04-.48-.11-.686a.502.502 0 0 0-.166-.761z" />
@@ -83,19 +83,19 @@
             </a>
         </div>
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
                 <i class="fa-solid fa-heart-crack"></i>
                 <div class="icon-caption">Terapia de Pareja</div>
             </a>
         </div>
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
                 <i class="fa-solid fa-fire-flame-curved"></i>
                 <div class="icon-caption">Sexualidad</div>
             </a>
         </div>
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-emoji-neutral" viewBox="0 0 16 16">
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
                     <path d="M4 10.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5m3-4C7 5.672 6.552 5 6 5s-1 .672-1 1.5S5.448 8 6 8s1-.672 1-1.5m4 0c0-.828-.448-1.5-1-1.5s-1 .672-1 1.5S9.448 8 10 8s1-.672 1-1.5" />
@@ -104,7 +104,7 @@
             </a>
         </div>
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-star-half" viewBox="0 0 16 16">
                     <path d="M5.354 5.119 7.538.792A.52.52 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.54.54 0 0 1 16 6.32a.55.55 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.5.5 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.6.6 0 0 1 .085-.302.51.51 0 0 1 .37-.245zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.56.56 0 0 1 .162-.505l2.907-2.77-4.052-.576a.53.53 0 0 1-.393-.288L8.001 2.223 8 2.226z" />
                 </svg>
@@ -112,13 +112,13 @@
             </a>
         </div>
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
                 <i class="fas fa-star"></i>
                 <div class="icon-caption">Sentido de vida</div>
             </a>
         </div>
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
                 <i class="fas fa-comments"></i>
                 <div class="icon-caption">Orientación
                     Vocacional</div>
@@ -126,21 +126,21 @@
         </div>
 
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
                 <i class="fa-solid fa-bed"></i>
                 <div class="icon-caption">Problemas
                     de sueño</div>
             </a>
         </div>
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
                 <i class="fa-solid fa-hand-holding-heart"></i>
                 <div class="icon-caption">Problemas
                     alimenticios</div>
             </a>
         </div>
         <div class="icon-item">
-            <a href="blog.php">
+            <a href="psicologos.php">
                 <i class="fa-solid fa-people-arrows"></i>
                 <div class="icon-caption">Relaciones
                     Interpersonales</div>
