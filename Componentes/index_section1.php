1ece3673397b142b6a070306c7002b89e2329396
Valec3 <v1c70rotakmer@gmail.com>
2024-08-20 13:10:09 -0500

    fix

--- Componentes/index_section1.php
+++ Componentes/index_section1.php
@@ -1,5 +1,5 @@
 <style>
-        /* :root {
+    /* :root {
             --color-light: #ffffff;
         }
 
@@ -15,117 +15,117 @@
             font-family: "Montserrat", sans-serif;
         }  */
 
+    .hero {
+        padding-top: 100px;
+        position: relative;
+        place-items: center;
+        height: 100vh;
+        max-height: 600px;
+        background-image: url("ContigoVoyAssets/recursos/Recursonuevo.jpg");
+        background-position: center;
+        background-size: cover;
+        display: grid;
+        .
+    }
+
+    .info {
+        position: relative;
+        margin-inline: auto;
+        display: flex;
+        justify-content: flex-end;
+        border: 2px solid black;
+        text-decoration: none;
+        color: #FFFFFF;
+        background-color: transparent;
+        border-radius: 0.5em;
+        font-weight: bold;
+        padding: 1em 2em;
+        font-size: 1em;
+
+    }
+
+
+
+    .hero::before {
+        place-items: center;
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.4);
+        z-index: 1;
+    }
+
+    .hero__content {
+        place-items: center;
+        position: relative;
+        z-index: 2;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+        gap: 1.5em;
+        color: var(--color-light);
+        padding: 0 1em;
+    }
+
+    .hero__title {
+        font-size: 3.5em;
+        margin: 0;
+        padding: 0;
+        place-items: center;
+    }
+
+    .hero__subtitle {
+        place-items: center;
+        font-size: 1.3em;
+        margin: 0;
+        padding: 0;
+    }
+
+    .hero__cta {
+        place-items: center;
+        text-transform: uppercase;
+        text-decoration: none;
+        color: var(--color-light);
+        background-color: var(--color-light-pink);
+        border-radius: 0.5em;
+        font-weight: bold;
+        padding: 1em 2em;
+        font-size: 0.8em;
+        transition: all 0.25s ease-in-out;
+    }
+
+    .hero__cta:hover {
+        color: var(--color-light-pink);
+        background-color: var(--color-light);
+    }
+
+    @media (max-width: 1920px) {
+        .info {
+            top: -70px;
+        }
+    }
+
+    @media (max-width: 1300px) {
+        .info {
+            top: auto;
+        }
+    }
+
+    @media (max-width:900px) {
+        .info {
+            top: auto;
+        }
+
         .hero {
-            padding-top: 100px;
-            position: relative;
-            place-items: center;
-            height: 100vh;
-            max-height: 600px;
-            background-image: url("ContigoVoyAssets/recursos/Recursonuevo.jpg");
-            background-position: center;
-            background-size: cover;
-            display: grid;.
-        }
-        .info{
-            position:relative;
-            left:500px; 
-            display:flex;
-            justify-content: flex-end;
-            border: 2px solid black;
-            text-decoration: none;
-            color: #FFFFFF;
-            background-color: transparent;
-            border-radius: 0.5em;
-            font-weight: bold;
-            padding: 1em 2em;
-            font-size: 1em;
-            
-        }
-        
-        
-
-        .hero::before {
-            place-items: center;
-            content: "";
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background-color: rgba(0, 0, 0, 0.4);
-            z-index: 1;
+            max-height: 1000px;
         }
 
-        .hero__content {
-            place-items: center;
-            position: relative;
-            z-index: 2;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            text-align: center;
-            gap: 1.5em;
-            color: var(--color-light);
-            padding: 0 1em;
-        }
-
-        .hero__title {
-            font-size:3.5em;
-            margin: 0;
-            padding: 0;
-            place-items: center;
-        }
-
-        .hero__subtitle {
-            place-items: center;
-            font-size: 1.3em;
-            margin: 0;
-            padding: 0;
-        }
-
-        .hero__cta {
-            place-items: center;
-            text-transform: uppercase;
-            text-decoration: none;
-            color: var(--color-light);
-            background-color: var(--color-light-pink);
-            border-radius: 0.5em;
-            font-weight: bold;
-            padding: 1em 2em;
-            font-size: 0.8em;
-            transition: all 0.25s ease-in-out;
-        }
-
-        .hero__cta:hover {
-            color: var(--color-light-pink);
-            background-color: var(--color-light);
-        }
-        @media (max-width: 1920px) {
-            .info{                
-                top:-70px; 
-            }
-        }
-        
-         @media (max-width: 1300px) {
-            .info{                
-                top:auto; 
-                left: 300px;
-            }
-         }
-        
-        @media (max-width:900px) {
-            .info{
-                left: auto;
-                top:auto;
-            }
-            .hero{
-                max-height: 1000px;
-            }
-            
-        }    
-        
-        
-    </style>
+    }
+</style>
 
 <body>
     <section class="hero">
@@ -139,11 +139,11 @@
                 y bienestar emocional aquí
             </h2>
             <a href="psicologos.php" class="hero__cta">Solicitar Servicio</a>
-            <a href="psicologos.php"class="info">Mas información aqui..</a>
+            <a href="psicologos.php" class="info">Mas información aqui..</a>
         </div>
-        
+
     </section>
-<!-- 
+    <!-- 
     <style>
         :root {
             --color-light: #ffffff;
