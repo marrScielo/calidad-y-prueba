b7ef1e8edeea00677139bfaaf9cf3358c2eb1d65
Victor Raul <v1c70rotakmer@gmail.com>
2024-08-26 11:19:52 -0500

    chore: Update page title and meta tags for SEO optimization

--- index.php
+++ index.php
@@ -4,11 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ContigoVoy</title>
+    <title>ContigoVoy - Psicólogos en Perú | Terapia Online y Presencial</title>
     <meta name="description"
-        content="Encuentra apoyo emocional y psicológico con nuestro psicólogo profesional. Ofrecemos terapia online y presencial. Reserva tu cita ahora y comienza tu camino hacia el bienestar.">
+        content="ContigoVoy ofrece apoyo emocional y psicológico con psicólogos profesionales en Perú. Disponemos de terapia online y presencial para tu bienestar. Reserva tu cita ahora y mejora tu salud mental.">
     <meta name="keywords"
-        content="psicólogo, terapia, apoyo emocional, salud mental, terapia online, terapia presencial, citas, reservas, bienestar emocional">
+        content="psicólogos Perú, psicólogos, terapia online, terapia presencial, apoyo emocional, salud mental, bienestar emocional, citas con psicólogos, ayuda psicológica, terapia en Perú">
+    <!-- 
+        GOOGLE SEARCH CONSOLE
+    -->
+    <meta name="google-site-verification" content="rd_HuMZMXyzCAnm4KNJ4dxbKlKt58T1iOTnnM0iJUss" />
     <link rel="icon" href="img/favicon.png">
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/styles.css">
@@ -20,12 +24,12 @@
     <link rel="stylesheet" href="css/indexseccion.css">
     <link rel="stylesheet" href="css/estilos-footer.css">
     <!-- <link rel="stylesheet" href="./css/styles.min.css"> -->
-    <!--  Iconos de Font Awesome -->
+    <!-- Iconos de Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <!-- OG -->
-    <meta property="og:title" content="ContigoVoy">
+    <!-- Open Graph Meta Tags -->
+    <meta property="og:title" content="ContigoVoy - Psicólogos en Perú | Terapia Online y Presencial">
     <meta property="og:description"
-        content="Encuentra apoyo emocional y psicológico con nuestro psicólogo profesional. Ofrecemos terapia online y presencial. Reserva tu cita ahora y comienza tu camino hacia el bienestar.">
+        content="ContigoVoy ofrece apoyo emocional y psicológico con psicólogos profesionales en Perú. Disponemos de terapia online y presencial para tu bienestar. Reserva tu cita ahora y mejora tu salud mental.">
     <meta property="og:image"
         content="https://res.cloudinary.com/drubtqo3l/image/upload/v1724082656/contigo-voy/dbcczle5tvaq8fpptf8f.png">
     <meta property="og:url" content="https://contigovoy.com/">
@@ -34,7 +38,6 @@
     <meta property="og:locale" content="es_ES">
     <meta property="og:image:width" content="1200">
     <meta property="og:image:height" content="630">
-
 </head>
 
 <body>
