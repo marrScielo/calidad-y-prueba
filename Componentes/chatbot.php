daf8748cb534ea1777e8746904b268cd9ab63f5c
Luis Santos <luisdemaryori@gmail.com>
2024-12-20 10:06:51 -0500

    new chatbot

---
+++ Componentes/chatbot.php
@@ -0,0 +1,12 @@
+<script>
+    window.embeddedChatbotConfig = {
+        chatbotId: "vB61FAkdNgR5-OlbMRQEZ",
+        domain: "www.chatbase.co"
+    }
+</script>
+<script
+    src="https://www.chatbase.co/embed.min.js"
+    chatbotId="vB61FAkdNgR5-OlbMRQEZ"
+    domain="www.chatbase.co"
+    defer>
+</script>
