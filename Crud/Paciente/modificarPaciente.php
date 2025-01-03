edc56cff86fe60a9bb2a5bfe27fa0c9011ce1af2
antcube <aqr885@gmail.com>
2025-01-03 16:31:20 -0500

    Rutas para editar paciente y cita

--- Crud/Paciente/modificarPaciente.php
+++ Crud/Paciente/modificarPaciente.php
@@ -1,12 +1,6 @@
 <?php
-//local
-// /home3/ghxumdmy/public_html/website_1cf5dd5d
-require_once("../../Controlador/Paciente/ControllerPaciente.php");
 
-//hosting
-//$ruta_local = "/Controlador/Paciente/ControllerPaciente.php";
-//$ruta_hosting = "/home3/ghxumdmy/public_html/website_1cf5dd5d";
-//require_once($ruta_hosting . $ruta_local);
+require_once("../../Controlador/Paciente/ControllerPaciente.php");
 
 $obj = new usernameControlerPaciente();
 
